70e2e61d9c96304ee87ee2fe8913f17f0dfe43a4
ilhamkhalik05 <believerrr05@gmail.com>
2025-11-18 09:47:28 +0700

    feat: Create project header

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    printf("First commit\n");
+void header();
+
+int main()
+{
+    header();
     return 0;
+}
+
+/**
+ * Prints the header of the program.
+ *
+ * This function prints the header of the program,
+ * which includes the title of the program and
+ * a separator line.
+ */
+void header()
+{
+    printf("=============================================================\n");
+    printf("========== Program Simulasi Manajemen Toko Sembako ==========\n");
+    printf("=============================================================\n");
 }

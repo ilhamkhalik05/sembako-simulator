f2bee7e433bd525222dc3a6884051a937c0439d9
ilhamkhalik05 <believerrr05@gmail.com>
2025-11-18 10:58:19 +0700

    refactor: Dokumentasi function dan memisahkan modul Produk dan Transaksi

--- src/main.c
+++ src/main.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void productModule();
+void transactionModule();
 void header();
 void headerModule(char *moduleName);
 bool isValidChoice(int start, int end, int choice);
 
 int main()
 {
-    int repeat = 1;
-    while (repeat)
-    {
-        const int startMainModule = 1;
-        const int endMainModule = 2;
-        int selectedMainModule = 0;
+    int selectedMainModule = 0;
+    const int startMainModule = 1;
+    const int endMainModule = 3;
 
+    // Akan terus mengulang program,
+    // jika user tidak memilih opsi terakhir
+    while (selectedMainModule != endMainModule)
+    {
+        // Header program
         header();
 
+        // Meminta user memilih modul utama
         do
         {
             printf("\n");
@@ -23,6 +28,7 @@ int main()
             printf("Pilih modul yang tersedia:\n");
             printf("1. Produk\n");
             printf("2. Transaksi\n");
+            printf("3. Keluar program\n");
 
             printf("Pilih berdasarkan angka (%d-%d): ", startMainModule, endMainModule);
             scanf("%d", &selectedMainModule);
@@ -33,72 +39,97 @@ int main()
         switch (selectedMainModule)
         {
         case 1:
-            int selectedProductSubModule = 0;
-            const int startProductSubModule = 1;
-            const int endProductSubModule = 6;
-
-            while (selectedProductSubModule != endProductSubModule)
-            {
-                do
-                {
-                    printf("\n");
-                    headerModule("Produk");
-
-                    printf("Pilih submodul yang tersedia:\n");
-                    printf("1. Lihat semua produk\n");
-                    printf("2. Lihat produk berdasarkan kategori\n");
-                    printf("3. Tambah produk baru\n");
-                    printf("4. Ubah data produk\n");
-                    printf("5. Hapus produk\n");
-                    printf("6. Keluar modul\n");
-
-                    printf("Pilih berdasarkan angka (%d-%d): ", startProductSubModule, endProductSubModule);
-                    scanf("%d", &selectedProductSubModule);
-                } while (!isValidChoice(startProductSubModule, endProductSubModule, selectedProductSubModule));
-
-                printf("\nPASSED\n");
-            }
-
+            productModule();
             break;
         case 2:
-            int selectedTransactionSubModule = 0;
-            const int startTransactionSubModule = 1;
-            const int endTransactionSubModule = 3;
-
-            while (selectedTransactionSubModule != endTransactionSubModule)
-            {
-                do
-                {
-                    printf("\n");
-                    headerModule("Transaksi");
-
-                    printf("Pilih submodul yang tersedia:\n");
-                    printf("1. Buat transaksi baru\n");
-                    printf("2. Lihat seluruh history transaksi\n");
-                    printf("3. Keluar modul\n");
-
-                    printf("Pilih berdasarkan angka (%d-%d): ", startTransactionSubModule, endTransactionSubModule);
-                    scanf("%d", &selectedTransactionSubModule);
-                } while (!isValidChoice(startTransactionSubModule, endTransactionSubModule, selectedTransactionSubModule));
-
-                printf("\nPASSED\n");
-            }
+            transactionModule();
             break;
-
         default:
             break;
         }
     }
 
+    printf("Program akan berakhir...\n\n");
+
     return 0;
 }
 
 /**
- * Prints the header of the program.
+ * Modul produk.
  *
- * This function prints the header of the program,
- * which includes the title of the program and
- * a separator line.
+ * Modul ini berisi fungsi-fungsi yang berhubungan dengan produk,
+ * seperti menampilkan daftar produk, menambah produk baru,
+ * mengubah data produk, menghapus produk, dan lain sebagainya.
+ */
+void productModule()
+{
+    int selectedProductSubModule = 0;
+    const int startProductSubModule = 1;
+    const int endProductSubModule = 6;
+
+    while (selectedProductSubModule != endProductSubModule)
+    {
+        // Meminta user memilih submodul Produk
+        do
+        {
+            printf("\n");
+            headerModule("Produk");
+
+            printf("Pilih submodul yang tersedia:\n");
+            printf("1. Lihat semua produk\n");
+            printf("2. Lihat produk berdasarkan kategori\n");
+            printf("3. Tambah produk baru\n");
+            printf("4. Ubah data produk\n");
+            printf("5. Hapus produk\n");
+            printf("6. Keluar modul\n");
+
+            printf("Pilih berdasarkan angka (%d-%d): ", startProductSubModule, endProductSubModule);
+            scanf("%d", &selectedProductSubModule);
+        } while (!isValidChoice(startProductSubModule, endProductSubModule, selectedProductSubModule));
+
+        printf("\nPASSED\n");
+    }
+}
+
+/**
+ * Modul transaksi.
+ *
+ * Modul ini berisi fungsi-fungsi yang berhubungan dengan transaksi,
+ * seperti membuat transaksi baru, melihat seluruh history transaksi,
+ * dan lain sebagainya.
+ */
+void transactionModule()
+{
+    int selectedTransactionSubModule = 0;
+    const int startTransactionSubModule = 1;
+    const int endTransactionSubModule = 3;
+
+    while (selectedTransactionSubModule != endTransactionSubModule)
+    {
+        // Meminta user memilih submodul Transaksi
+        do
+        {
+            printf("\n");
+            headerModule("Transaksi");
+
+            printf("Pilih submodul yang tersedia:\n");
+            printf("1. Buat transaksi baru\n");
+            printf("2. Lihat seluruh history transaksi\n");
+            printf("3. Keluar modul\n");
+
+            printf("Pilih berdasarkan angka (%d-%d): ", startTransactionSubModule, endTransactionSubModule);
+            scanf("%d", &selectedTransactionSubModule);
+        } while (!isValidChoice(startTransactionSubModule, endTransactionSubModule, selectedTransactionSubModule));
+
+        printf("\nPASSED\n");
+    }
+}
+
+/**
+ * Menampilkan header program.
+ *
+ * Fungsi ini menampilkan header program yang
+ * mencakup judul program dan garis pemisah.
  */
 void header()
 {
@@ -108,13 +139,12 @@ void header()
 }
 
 /**
- * Prints the header of a module
+ * Menampilkan header dari sebuah modul.
  *
- * This function prints the header of a module,
- * which includes the name of the module and
- * a separator line.
+ * Fungsi ini menampilkan header dari sebuah modul,
+ * mencakup nama modul dan garis pemisah.
  *
- * @param moduleName the name of the module
+ * @param moduleName nama modul
  */
 void headerModule(char *moduleName)
 {
@@ -122,23 +152,15 @@ void headerModule(char *moduleName)
 }
 
 /**
- * Checks if the given choice is valid
- * within the range of start and end
+ * Mengecek apakah pilihan yang diberikan valid
+ * berdasarkan rentang nilai `start` dan `end`.
  *
- * @param start the start of the range
- * @param end the end of the range
- * @param choice the choice to be validated
- * @return true if the choice is valid, false otherwise
+ * @param start nilai awal rentang
+ * @param end nilai akhir rentang
+ * @param choice pilihan yang ingin divalidasi
+ * @return true jika pilihan valid, false jika tidak
  */
 bool isValidChoice(int start, int end, int choice)
 {
-    for (int i = start; i <= end; i++)
-    {
-        if (i == choice)
-        {
-            return true;
-        }
-    }
-
-    return false;
+    return choice >= start && choice <= end;
 }

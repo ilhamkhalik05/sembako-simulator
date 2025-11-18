a7c7179d72dd7ecd40635406d5f631ebc492aa87
ilhamkhalik05 <believerrr05@gmail.com>
2025-11-18 10:27:55 +0700

    feat: Scratch program workflow (masih ada bug)

--- src/main.c
+++ src/main.c
@@ -2,27 +2,94 @@
 #include <stdbool.h>
 
 void header();
+void headerModule(char *moduleName);
 bool isValidChoice(int start, int end, int choice);
 
 int main()
 {
-    int selectedMainModule;
-
-    header();
-
-    do
+    int repeat = 1;
+    while (repeat)
     {
-        printf("\n");
+        const int startMainModule = 1;
+        const int endMainModule = 2;
+        int selectedMainModule = 0;
 
-        printf("Pilih modul yang tersedia:\n");
-        printf("1. Produk\n");
-        printf("2. Transaksi\n");
+        header();
 
-        printf("Pilih berdasarkan angka (1-2): ");
-        scanf("%d", &selectedMainModule);
-    } while (!isValidChoice(1, 2, selectedMainModule));
+        do
+        {
+            printf("\n");
 
-    printf("\nPASSED\n");
+            printf("Pilih modul yang tersedia:\n");
+            printf("1. Produk\n");
+            printf("2. Transaksi\n");
+
+            printf("Pilih berdasarkan angka (%d-%d): ", startMainModule, endMainModule);
+            scanf("%d", &selectedMainModule);
+        } while (!isValidChoice(startMainModule, endMainModule, selectedMainModule));
+
+        printf("\nPASSED\n");
+
+        switch (selectedMainModule)
+        {
+        case 1:
+            int selectedProductSubModule = 0;
+            const int startProductSubModule = 1;
+            const int endProductSubModule = 6;
+
+            while (selectedProductSubModule != endProductSubModule)
+            {
+                do
+                {
+                    printf("\n");
+                    headerModule("Produk");
+
+                    printf("Pilih submodul yang tersedia:\n");
+                    printf("1. Lihat semua produk\n");
+                    printf("2. Lihat produk berdasarkan kategori\n");
+                    printf("3. Tambah produk baru\n");
+                    printf("4. Ubah data produk\n");
+                    printf("5. Hapus produk\n");
+                    printf("6. Keluar modul\n");
+
+                    printf("Pilih berdasarkan angka (%d-%d): ", startProductSubModule, endProductSubModule);
+                    scanf("%d", &selectedProductSubModule);
+                } while (!isValidChoice(startProductSubModule, endProductSubModule, selectedProductSubModule));
+
+                printf("\nPASSED\n");
+            }
+
+            break;
+        case 2:
+            int selectedTransactionSubModule = 0;
+            const int startTransactionSubModule = 1;
+            const int endTransactionSubModule = 3;
+
+            while (selectedTransactionSubModule != endTransactionSubModule)
+            {
+                do
+                {
+                    printf("\n");
+                    headerModule("Transaksi");
+
+                    printf("Pilih submodul yang tersedia:\n");
+                    printf("1. Buat transaksi baru\n");
+                    printf("2. Lihat seluruh history transaksi\n");
+                    printf("3. Keluar modul\n");
+
+                    printf("Pilih berdasarkan angka (%d-%d): ", startTransactionSubModule, endTransactionSubModule);
+                    scanf("%d", &selectedTransactionSubModule);
+                } while (!isValidChoice(startTransactionSubModule, endTransactionSubModule, selectedTransactionSubModule));
+
+                printf("\nPASSED\n");
+            }
+            break;
+
+        default:
+            break;
+        }
+    }
+
     return 0;
 }
 
@@ -38,6 +105,20 @@ void header()
     printf("=============================================================\n");
     printf("========== Program Simulasi Manajemen Toko Sembako ==========\n");
     printf("=============================================================\n");
+}
+
+/**
+ * Prints the header of a module
+ *
+ * This function prints the header of a module,
+ * which includes the name of the module and
+ * a separator line.
+ *
+ * @param moduleName the name of the module
+ */
+void headerModule(char *moduleName)
+{
+    printf("====== Modul %s ======\n", moduleName);
 }
 
 /**
